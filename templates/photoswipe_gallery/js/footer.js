7eabc47ac81e986fa0102cd52cff18867d28ce42
Ruben Barkow-Kuder <github@r.z11.de>
2026-04-23 16:36:03 +0200

    fix: allow directory navigation by deferring preventDefault until after directory check
    Moved preventDefault call after directory detection to enable default link
    behavior for folders (data-size="x"). Prevents PhotoSwipe from intercepting
    directory clicks while still blocking default behavior for image thumbnails.

--- templates/photoswipe_gallery/js/footer.js
+++ templates/photoswipe_gallery/js/footer.js
@@ -63,8 +63,6 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
     // triggers when user clicks on thumbnail
     var onThumbnailsClick = function(e) {
         e = e || window.event;
-        e.preventDefault ? e.preventDefault() : e.returnValue = false;
-
         var eTarget = e.target || e.srcElement;
 
         // find root element of slide
@@ -75,6 +73,15 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
         if(!clickedListItem) {
             return;
         }
+
+        // Check if this is a directory (data-size="x")
+        var linkEl = clickedListItem.children[0];
+        if (linkEl && linkEl.getAttribute('data-size') === 'x') {
+            // Allow default click behavior for directories
+            return;
+        }
+
+        e.preventDefault ? e.preventDefault() : e.returnValue = false;
 
         // find index of clicked item by looping through all child nodes
         // alternatively, you may define index via data- attribute
